0606ad17f8f169233fab53ff83705d2d9b36a047
Anastasia <pakgeomart@gmail.com>
2022-08-04 01:18:42 +0200

    HM4

---
+++ ДЗ_4/Program.cs
@@ -0,0 +1,38 @@
+﻿//=========Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*int FindSum (int a) 
+{
+    int sum =0; 
+    while(a>0)
+    {   
+        sum=sum+a%10;
+        a=a/10;
+  
+    }
+    return sum;
+}
+    
+Console.WriteLine("Input positive num: ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of numbers  {num} is {FindSum(num)}"); */
+
+// ======Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+void ArrayElem(int num) //определим массив
+{
+     int[] array = new int[num];    
+    
+    int index=0;
+    while(index<num)
+    {
+        array[index]=new Random().Next(1,10);
+        Console.Write(" ");
+        Console.Write(array[index]);
+        index++;
+    }
+        
+}
+
+Console.Write("Imput:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+ArrayElem(m);
